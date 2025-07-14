ccde9eb4faa3b1950f584e3b2d72cfd14c3bf7c9
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-07-14 20:46:25 -0300

    Create Interface of CRUD

---
+++ src/main/java/com/nicolas/project/dao/CrudDAO.java
@@ -0,0 +1,13 @@
+package com.nicolas.project.dao;
+
+import java.util.List;
+
+public interface CrudDAO<T> {
+    public void create(T obj);
+
+    public List<T> read();
+
+    public void update(T obj);
+
+    public void delete(int id);
+}
