2b8376961d4dad0f6e09ed4090ecc8cf34279405
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-07-15 20:56:14 -0300

    Testing function CREATE of Category

--- src/main/java/com/nicolas/project/app/Main.java
+++ src/main/java/com/nicolas/project/app/Main.java
@@ -3,15 +3,17 @@ package com.nicolas.project.app;
 import java.sql.Connection;
 
 import com.nicolas.project.config.MySQLConnection;
+import com.nicolas.project.dao.CrudCategory;
+import com.nicolas.project.models.Category;
 
 public class Main {
     public static void main(String[] args) {
+        CrudCategory crudCat = new CrudCategory();
+        Category category = new Category("Food");
 
-        try (Connection test = MySQLConnection.getConnection()) {
-            System.out.println("Connected");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        crudCat.create(category);
 
+        System.out.println(category);
+        System.out.println(category.getId());
     }
 }
