d75f8383439395f8f30679bfdd32d465796ca999
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-07-21 20:49:27 -0300

    Testing function READ of category

--- src/main/java/com/nicolas/project/app/Main.java
+++ src/main/java/com/nicolas/project/app/Main.java
@@ -1,19 +1,11 @@
 package com.nicolas.project.app;
 
-import java.sql.Connection;
-
-import com.nicolas.project.config.MySQLConnection;
 import com.nicolas.project.dao.CrudCategory;
-import com.nicolas.project.models.Category;
 
 public class Main {
     public static void main(String[] args) {
         CrudCategory crudCat = new CrudCategory();
-        Category category = new Category("Food");
 
-        crudCat.create(category);
-
-        System.out.println(category);
-        System.out.println(category.getId());
+        System.out.println(crudCat.read());
     }
 }
