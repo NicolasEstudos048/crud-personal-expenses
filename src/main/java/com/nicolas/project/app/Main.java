107a6994e878c8e55c8af83a51e782f12c3156ea
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-07-21 21:14:39 -0300

    Testing function UPDATE of category

--- src/main/java/com/nicolas/project/app/Main.java
+++ src/main/java/com/nicolas/project/app/Main.java
@@ -1,10 +1,14 @@
 package com.nicolas.project.app;
 
 import com.nicolas.project.dao.CrudCategory;
+import com.nicolas.project.models.Category;
 
 public class Main {
     public static void main(String[] args) {
         CrudCategory crudCat = new CrudCategory();
+        Category category = new Category(1, "Phone");
+
+        crudCat.update(category);
 
         System.out.println(crudCat.read());
     }
