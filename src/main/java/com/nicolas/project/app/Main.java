2a70ab43628f5033e5a81a5be6927f1de9dd2fa7
NicolasEstudos048 <estudosnicolas2@gmail.com>
2025-07-21 21:25:38 -0300

    Testing function DELETE of Category

--- src/main/java/com/nicolas/project/app/Main.java
+++ src/main/java/com/nicolas/project/app/Main.java
@@ -1,15 +1,13 @@
 package com.nicolas.project.app;
 
 import com.nicolas.project.dao.CrudCategory;
-import com.nicolas.project.models.Category;
 
 public class Main {
     public static void main(String[] args) {
         CrudCategory crudCat = new CrudCategory();
-        Category category = new Category(1, "Phone");
 
-        crudCat.update(category);
-
+        System.out.println(crudCat.read());
+        crudCat.delete(1);
         System.out.println(crudCat.read());
     }
 }
